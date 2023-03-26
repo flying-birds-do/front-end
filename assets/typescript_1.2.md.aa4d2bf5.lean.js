1b6dff3120d9bf1e9b157b2db2d29129d8e52c3c
flying-birds-do <flying-birds-do@users.noreply.github.com>
2023-03-26 13:52:05 +0000

    deploy: a2fa269bcb9c6ac9667b4d1b56f1ff61e41fd690

---
+++ assets/typescript_1.2.md.aa4d2bf5.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as l}from"./app.df71334e.js";const i=JSON.parse('{"title":"变量声明","description":"","frontmatter":{},"headers":[],"relativePath":"typescript/1.2.md"}'),p={name:"typescript/1.2.md"},o=l("",101),e=[o];function t(c,r,y,F,D,C){return a(),n("div",null,e)}const u=s(p,[["render",t]]);export{i as __pageData,u as default};
--- assets/typescript_1.2.md.bdc163cb.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as n,o as a,a as l}from"./app.ec6b9669.js";const i=JSON.parse('{"title":"变量声明","description":"","frontmatter":{},"headers":[],"relativePath":"typescript/1.2.md"}'),p={name:"typescript/1.2.md"},o=l("",101),e=[o];function t(c,r,y,F,D,C){return a(),n("div",null,e)}const u=s(p,[["render",t]]);export{i as __pageData,u as default};
